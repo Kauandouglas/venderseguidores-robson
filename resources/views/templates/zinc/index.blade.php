1caa87b2c0cca2e4998ba54c054a80c94e9fa28a
Kauan <kauandouglas737@gmail.com>
2025-12-24 17:54:55 -0300

    update

--- resources/views/templates/zinc/index.blade.php
+++ resources/views/templates/zinc/index.blade.php
@@ -27,7 +27,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    @foreach($template['services'] as $index => $service)
+                    @foreach($template['services'] ?? [] as $index => $service)
                         <div class="card">
                             <div class="card-icon {{ ['blue', 'yellow', 'red'][$index % 3] }}"
      style="background: url('{{ !empty($service['image'])
