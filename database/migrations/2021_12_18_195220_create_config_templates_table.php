fd8653b6b776bba5c0eab15e6093aa85b4433fb1
Kauan <kauandouglas737@gmail.com>
2025-12-24 12:40:41 -0300

    update

--- database/migrations/2021_12_18_195220_create_config_templates_table.php
+++ database/migrations/2021_12_18_195220_create_config_templates_table.php
@@ -13,53 +13,19 @@ class CreateConfigTemplatesTable extends Migration
      */
     public function up()
     {
-        Schema::create('config_templates', function (Blueprint $table) {
+       Schema::create('config_templates', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->unique();
 
-            // Nav
-            $table->string('nav_button')->nullable();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->unique();
 
-            // Header
-            $table->string('header_title')->nullable();
-            $table->text('header_sub_title')->nullable();
-            $table->string('header_button')->nullable();
-            $table->string('header_image')->nullable();
+            $table->json('content');
 
-            // Service
-            $table->string('service_image_1')->nullable();
-            $table->string('service_title_1')->nullable();
-            $table->text('service_sub_title_1')->nullable();
-            $table->text('service_description_1')->nullable();
-            $table->string('service_image_2')->nullable();
-            $table->string('service_title_2')->nullable();
-            $table->text('service_sub_title_2')->nullable();
-            $table->text('service_description_2')->nullable();
-            $table->string('service_image_3')->nullable();
-            $table->string('service_title_3')->nullable();
-            $table->text('service_sub_title_3')->nullable();
-            $table->text('service_description_3')->nullable();
-
-            // About
-            $table->string('about_image')->nullable();
-            $table->string('about_title')->nullable();
-            $table->text('about_description')->nullable();
-            $table->string('about_button')->nullable();
-
-            // Basic
-            $table->string('basic_title')->nullable();
-            $table->text('basic_description')->nullable();
-
-            // Contact
-            $table->string('contact_title')->nullable();
-            $table->text('contact_description')->nullable();
-
-            // Footer
-            $table->string('footer_title')->nullable();
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
+
     }
 
     /**
